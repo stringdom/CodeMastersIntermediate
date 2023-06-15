9c5fc1604c8aebe552dea31ed61be7360088ec2a
Leonardo Pérez <leo.prez.k@gmail.com>
2023-06-15 20:04:35 +0000

    State manager

---
+++ StateMachine/StateManager.cs
@@ -0,0 +1,11 @@
+namespace StateMachine;
+
+// Implements the State handling
+class StateManager
+{
+    private IState _state;
+    public StateManager (IState state)
+    {
+        _state = state;
+    }
+}
