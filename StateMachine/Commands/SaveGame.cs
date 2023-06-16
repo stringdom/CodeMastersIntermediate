3b2745b612b33ea12c139c4e50d4a4803e928e44
Leonardo Pérez <leo.prez.k@gmail.com>
2023-06-16 14:55:58 +0000

    Saving actually works now

--- StateMachine/Commands/SaveGame.cs
+++ StateMachine/Commands/SaveGame.cs
@@ -2,7 +2,7 @@
 
 public class SaveGame : ICommand
 {
-    string Path { get; set; } = "../SaveFiles/";
+    public string Path { get; set; } = "../SaveFiles/";
     string SaveName { get; set; } = "save1.txt";
 
     public SaveGame(string path, string saveFile)
@@ -12,7 +12,31 @@ public class SaveGame : ICommand
     }
     public void Execute()
     {
+        string fullPath = Path + SaveName;
+        if (File.Exists(fullPath))
+        {
+            string answer = StateManager.GetText("File already exists, want to overwrite (y/n): ");
+            if (answer.ToLower().Contains('n'))
+            {
+                return;
+            }
+        }
+        WriteFile(fullPath);
         WriteLine("Game saved to {0}{1}", Path, SaveName);
         ReadKey();
+    }
+    public void WriteFile(string fullPath)
+    {
+        Directory.CreateDirectory(Path);
+        try
+        {
+            using StreamWriter stream = File.CreateText(fullPath);
+            stream.WriteLine("File was saved correctly.");
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+
     }
 }
--- StateMachine/GlobalUsings.cs
+++ StateMachine/GlobalUsings.cs
@@ -1,3 +1,4 @@
 global using static System.Console;
 global using System;
 global using StateMachine;
+global using System.IO;
